bb210a08e38bdd3e2df0bffdaac5e25e0373ab11
Stormi Anduaga-Arias <stormarias@gmail.com>
2023-03-31 23:19:07 -0700

    Finds the year of a superbowl win.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+// Write a function called superbowlWin() in index.js:
+
+// The function should receive 1 argument, an Array of JavaScript Objects
+// Each object has two properties: year and result
+// It should use find() to test each Object to see if the result is "W" — a win!
+// It should return the year when the win occurred (if it occurred at all!)
+// If no win is found, it should return, sadly, undefined
+
+function isWin(element){
+    if(element.result === "W"){
+        return element;
+    }
+}
+
+function superbowlWin(record){
+    let year;
+  
+    let winObj = record.find(isWin);
+    if (winObj === undefined) {
+      return undefined;
+    }
+    
+    year = winObj.year;
+    return year;
+}
